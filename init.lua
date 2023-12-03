c178e5cc4917501d7bcda28aeab2dd175ac4b723
ag47silver <plantingpine@gmail.com>
2023-12-03 16:46:46 +0900

    Set highlight yank

--- init.lua
+++ init.lua
@@ -5,3 +5,11 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+-- Highlight on yank
+vim.cmd [[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+  augroup end
+]]
